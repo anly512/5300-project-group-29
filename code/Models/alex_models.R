409875137d910eae11e25504c918c0724e0843d3
Alex Pattarini <amp419@georgetown.edu>
2024-04-13 10:13:18 -0400

    model test

--- code/Models/alex_models.R
+++ code/Models/alex_models.R
@@ -12,7 +12,15 @@ library(gam)
 library(caret)
 
 # Contains regular season average stats for the winners and losers of all matchups in the NCAA tournament (2007-2019)
-df <- read_csv("./data/regular_season_stats_of_all_tournament_team_matchups_2007_2019.csv")
+df <- read_csv("./data/regular_season_stats_of_all_tournament_team_matchups_2007_2019.csv") %>%
+  drop_na()
+
+# Create variables with the difference in stats of winners and losers and the overall season stats
+df <- df %>%
+  mutate(victor_avg_points_season_plusminus = victor_avg_score - season_avg_score,
+         loser_avg_points_season_plusminus = loser_avg_score - season_avg_score,
+         victor_avg_opp_points_season_plusminus = victor_avg_opp_score - season_avg_opp_score,
+         loser_avg_opp_points_season_plusminus = loser_avg_opp_score - season_avg_opp_score)
 
 # Train-test-validation split
 set.seed(17)
@@ -27,9 +35,16 @@ rm(train_val, train_val_idx, val_idx)
 ### Logistic Regression ###
 # Outcome: Upset (1 if upset, 0 if not)
 
+# Predictors of interest
+preds <- c("upset",
+           "victor_avg_points_season_plusminus",
+           "loser_avg_points_season_plusminus",
+           "victor_avg_opp_points_season_plusminus",
+           "loser_avg_opp_points_season_plusminus")
 
-
-
+# Logistic model of mean regular season points scored compared to the average of all tournament teams
+model_logit <- glm(factor(upset) ~ ., data = train[preds], family = "binomial")
+summary(model_logit)
 
 
 
